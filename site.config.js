fc8dcf9ddf751d961bc1612114543b50fd4ed36b
Sebastian Cuellar <sebastiancuellar@pm.me>
2021-05-24 08:51:12 -0400

    seba blog config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Sebastian-Cuellar-8f93498376984eee8492a5f50811213f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Sebastian Cuellar',
+  domain: 'seba.lol',
+  author: 'Sebastian Cuellar',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  socialImageTitle: 'sebastiancuellar',
+  socialImageSubtitle: 'Hola 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'sebalilith',
+  github: 'sebastiancuellar',
+  linkedin: 'sebastiancuellar',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
